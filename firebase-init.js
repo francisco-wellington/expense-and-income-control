dab25c08e53113a0e927599d2330d06579c1c917
Wellington Silva <77394315+WellingtonSilva12@users.noreply.github.com>
2022-06-12 15:22:26 -0300

    Aplicação web, html, css, javascript e firebase
    A ideia principal do projeto é criar uma aplicação web para controle de entrada e saida de dinheiro de uma pessoa.

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCoiWwWIAyJDvAYTY5vz2t9sDObxkolw-4',
+  authDomain: 'expense-control-71e46.firebaseapp.com',
+  projectId: 'expense-control-71e46',
+  storageBucket: 'expense-control-71e46.appspot.com',
+  messagingSenderId: '372729509224',
+  appId: '1:372729509224:web:bc0bd2801d85d1bc2c12dd'
+}
+firebase.initializeApp(firebaseConfig)
